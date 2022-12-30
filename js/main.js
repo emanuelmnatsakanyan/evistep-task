8ef75d4ad6862cd7270742099003fdbccb632c0f
Emanuel Mnatsakanyan <mnatsakanyan343@gmail.com>
2022-12-30 20:31:20 +0400

    Testing

--- js/main.js
+++ js/main.js
@@ -13,10 +13,27 @@ const rewards = document.querySelectorAll('.reward')
 rewards.forEach((reward, i) => {
     if (i < rewards.length / 2) {
         reward.style.transform = `scaleY(${0.6 + ((i + 1) * 0.08)}) translateX(${30 - (i * 5)}px)`
-
     } else {
         reward.style.transform = `scaleY(${1.32 - ((i - 1) * 0.08)}) translateX(${(4 - i) * 5}px)`
         reward.style.zIndex = 4 - i
     }
 })
 
+let tabHeader = document.querySelector(".feature__tabs_header");
+let tabIndicator = document.querySelector(".feature__tabs_indicator");
+let tabBody = document.querySelector(".feature__tabs_body");
+
+
+let tabsPane = tabHeader.querySelectorAll(".feature__tab_switch");
+console.log(tabsPane);
+
+for (let i = 0; i < tabsPane.length; i++) {
+    tabsPane[i].addEventListener("click", function () {
+        tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+        tabsPane[i].classList.add("active");
+        tabBody.querySelector(".active").classList.remove("active");
+        tabBody.querySelectorAll(".feature__tab")[i].classList.add("active");
+
+        tabIndicator.style.left = `calc(calc(100% / 6) * ${i})`;
+    });
+}
