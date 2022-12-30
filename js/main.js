f203c6100526f933dc07b85e07578740e57a9c51
Emanuel Mnatsakanyan <mnatsakanyan343@gmail.com>
2022-12-30 14:11:50 +0400

    Fixed

--- js/main.js
+++ js/main.js
@@ -12,9 +12,10 @@ const rewards = document.querySelectorAll('.reward')
 
 rewards.forEach((reward, i) => {
     if (i < rewards.length / 2) {
-        reward.style.transform = `scaleY(${0.6 + ((i + 1) * 0.08)}) translateX(${50 - (i * 10)}px)`
+        reward.style.transform = `scaleY(${0.6 + ((i + 1) * 0.08)}) translateX(${30 - (i * 5)}px)`
+
     } else {
-        reward.style.transform = `scaleY(${1.32 - ((i - 1) * 0.08)}) translateX(${(4 - i) * 10}px)`
+        reward.style.transform = `scaleY(${1.32 - ((i - 1) * 0.08)}) translateX(${(4 - i) * 5}px)`
         reward.style.zIndex = 4 - i
     }
 })
